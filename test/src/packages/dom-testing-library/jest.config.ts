8efd68e7127c34aaa30a8660bd57f046213dd08c
lisachandra <lisachandra@proton.me>
2025-04-27 20:12:16 +0700

    test: remove unused tests

--- test/src/packages/dom-testing-library/jest.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from "@rbxts/jest";
-
-export = {
-	displayName: "dom-testing-library",
-	testMatch: ["**/*.spec"],
-	setupFilesAfterEnv: [script.Parent!.WaitForChild<ModuleScript>("setup-env")],
-} satisfies Config;
--- test/src/packages/react-testing-library/jest.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from "@rbxts/jest";
-
-export = {
-	displayName: "react-testing-library",
-	testMatch: ["**/*.spec"],
-	setupFilesAfterEnv: [script.Parent!.WaitForChild<ModuleScript>("setup-env")],
-} satisfies Config;
