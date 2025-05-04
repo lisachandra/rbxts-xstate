d294e1c1a99a5198d7a7b8626a5b1524577e6393
lisachandra <lisachandra@proton.me>
2025-05-04 19:05:54 +0700

    chore: move unrelated tests to other repos

--- test/src/shared/abort-controller/jest.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from "@rbxts/jest";
-
-export = {
-	displayName: "abort-controller",
-	testMatch: ["**/*.spec"],
-} satisfies Config;
--- test/src/shared/event-target/jest.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from "@rbxts/jest";
-
-export = {
-	displayName: "event-target",
-	testMatch: ["**/*.spec"],
-} satisfies Config;
