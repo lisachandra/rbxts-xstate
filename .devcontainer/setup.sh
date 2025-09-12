d5fd6276f8b28bf96b0df8a2d6a033a1abd405ec
lisachandra <lisachandra@proton.me>
2025-09-12 18:19:46 +0700

    fix(ci): missing source command because of shebang and path errors

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+ROKIT_BIN_DIR="$HOME/.rokit/bin"
+export PATH="$PATH:$ROKIT_BIN_DIR"
+echo "$ROKIT_BIN_DIR" >> "$GITHUB_PATH"
+source ~/.bashrc
 
 curl -sSf https://raw.githubusercontent.com/rojo-rbx/rokit/main/scripts/install.sh | bash
-source ~/.bashrc
 rokit install --no-trust-check
 npm install -g corepack
 corepack enable
